e52a50f130d819569dbd0e58323157fd1b04d416
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-01-28 19:27:39 -0500

    Broke up initial app.js into multiple files

---
+++ src/app/component.service.js
@@ -0,0 +1,23 @@
+class ComponentService {
+    constructor() {
+        this.numberOneInput = document.getElementById("numberOne");
+        this.numberTwoInput = document.getElementById("numberTwo");
+        this.addValuesButton = document.getElementById("addValues");
+        this.resultDiv = document.getElementById("result");
+    }
+
+    // returns the input values
+    getInputs() {
+        return [this.numberOneInput.value, this.numberTwoInput.value];
+    }
+
+    // sets the result
+    setResult(str) {
+        this.resultDiv.innerText = str;
+    }
+
+    // Adds an click listener that runs cb
+    onClick(cb) {
+        this.addValuesButton.addEventListener("click", cb);
+    }
+}
